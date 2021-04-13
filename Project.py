eb3ce6c48b27d68c9829a5f86032c11e58074146
jaidevsanketi <79723328+jaidevsanketi@users.noreply.github.com>
2021-04-13 15:04:45 +0200

    PSAW stuff

--- Project.py
+++ Project.py
@@ -1,3 +1,17 @@
 print ("Hello, ADA team")
 
 print ("Add Code below")
+
+from psaw import PushshiftAPI
+import datetime
+
+api=PushshiftAPI()
+
+start_time= int(datetime.datetime(2021,3,30).timestamp())
+
+submissions=list(api.search_submissions(after=start_time,
+                            subreddit='wallstreetbets',
+                            filter=['url','author', 'title', 'subreddit'],
+                            limit=100))
+for submissions in submissions:
+    print(submissions)
