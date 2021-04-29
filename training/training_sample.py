1e28799271c97d779d8454ff4c74a6da2cb18d6e
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-04-29 13:17:36 +0200

    Split new sample

--- training/training_sample.py
+++ training/training_sample.py
@@ -9,15 +9,15 @@ import pandas as pd
 
 
 #The aim of this file is to create a training sample, 
-#to do so we'll get 200 top comments from 10 different days
+#to do so we'll get 100 top comments from 30 different days
 #and we'll set manually the mood associated to each comments
 
 #Create the Dataframe if not yet done
-"""
+
 
 from daily_WSB_data import get_daily_comments
 
-n = 200 #Number of comments per day to construct dataset
+n = 100 #Number of comments per day to construct dataset
 date1 = [27,4,2021]
 date2 = [26,4,2021]
 date3 = [23,4,2021]
@@ -28,8 +28,34 @@ date7 = [19,4,2021]
 date8 = [16,4,2021]
 date9 = [15,4,2021]
 date10 = [14,4,2021]
+date11 = [13,4,2021]
+date12 = [12,4,2021]
+date13 = [9,4,2021]
+date14 = [8,4,2021]
+date15 = [7,4,2021]
+date16 = [6,4,2021]
+date17 = [5,4,2021]
+date18 = [1,4,2021]
+date19 = [31,3,2021]
+date20 = [30,3,2021]
+date21 = [26,3,2021]
+date22 = [25,3,2021]
+date23 = [24,3,2021]
+date24 = [23,3,2021]
+date25 = [22,3,2021]
+date26 = [19,3,2021]
+date27 = [18,3,2021]
+date28 = [17,3,2021]
+date29 = [16,3,2021]
+date30 = [15,3,2021]
+
+
+
 #Dates to create training sample on
-dates = [date1, date2, date3, date4, date5, date6, date7, date8, date9, date10]
+dates = [date1, date2, date3, date4, date5, date6, date7, date8, date9, date10,
+         date11, date12, date13, date14, date15, date16, date17, date18, date19, date20,
+         date21, date22, date23, date24, date25, date26, date27, date28, date29, date30]
+
 
 #Create the dataset 
 training_df = pd.DataFrame(columns= ['id', 'body', 'score'])
@@ -39,47 +65,22 @@ for d in dates:
     month = d[1]
     year = d[2]
     
-    temp_df = get_daily_comments(day, month, year, n, 3)
+    temp_df = get_daily_comments(day, month, year, n, 2)
     
     training_df = training_df.append(temp_df, ignore_index=True)
 
-"""
+#Create the mood column
+training_df['mood'] = ''
 
-# (1) Load dataframe if already created
-training_df = pd.read_csv(r'C:\Users\Antoine\Desktop\MScF\AdvancedDataAnalysis\Project\training_data.csv', sep='\t')
+#Split dataset
+training_df_1 = training_df[:1000]
+training_df_2 = training_df[1000:2000]
+training_df_3 = training_df[2000:]
 
-
-#Add column Mood (Bearish (=S), Bullish (=L), Neutral (=N), Delete (=Del)) #CHECK IF ALREADY EXISTS BEFORE DOING !!!!
-#training_df['mood'] = ''
-
-# (2) Allows to display full message in terminal (Can still increase if needed)
-pd.options.display.max_colwidth = 300
-
-# (3) Loop going through comments :
-    #- Type "L" if mood is bullish
-    #- Type "S" if mood is bearish
-    #- Type "N" if mood is neutral
-    #- Type "Del" if the comment isn't useful and we'll delete it late
-    #- Type "stop" if you want to quit the loop
-for i in training_df['id']:
-    print(training_df[(training_df['id'] == i)]['body'])
-    
-    user_mood = input('Enter mood: ')
-    
-    while not user_mood  in ['L', 'S', 'N', 'Del', 'stop']:
-        user_mood = input('Enter mood: ')
-    
-    if user_mood == ' ':
-        continue
-    elif user_mood == 'stop':
-        break
-    else :
-        #training_df[(training_df['id'] == i)]['mood'] = user_mood
-        training_df.loc[training_df['id'] == i, ['mood']] = user_mood
-
-# (4) After exiting the loop, delete the "Del" comments
-training_df = training_df[(training_df['mood'] != 'Del')]
-
-
-# (5) Save the updated DF to .csv and don't forget to update it on GIT
+#Save datasets as .csv
 training_df.to_csv(r'C:\Users\Antoine\Desktop\MScF\AdvancedDataAnalysis\Project\training_data.csv', index=False, sep='\t') #Save df into .csv with tab separation
+
+training_df_1.to_csv(r'C:\Users\Antoine\Desktop\MScF\AdvancedDataAnalysis\Project\training_data_1.csv', index=False, sep='\t') #Save df into .csv with tab separation
+training_df_2.to_csv(r'C:\Users\Antoine\Desktop\MScF\AdvancedDataAnalysis\Project\training_data_2.csv', index=False, sep='\t') #Save df into .csv with tab separation
+training_df_3.to_csv(r'C:\Users\Antoine\Desktop\MScF\AdvancedDataAnalysis\Project\training_data_3.csv', index=False, sep='\t') #Save df into .csv with tab separation
+
