e4fc4319c94cae3687b868b4a4b21598a1aa576c
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-06-04 14:42:04 +0200

    Update daily_analysis.py

--- daily_analysis.py
+++ daily_analysis.py
@@ -10,11 +10,12 @@ from daily_functions import classify_comments, daily_stats
 import tensorflow as tf
 
 
+
 ############################
 ######## PARAMETERS ########
 ############################
 
-date = [20,5,2021]
+date = [3,6,2021] 
 
 n = 3000 #Number of comments 
 m_c = 32 #Number of "more comments" replaced
@@ -32,20 +33,20 @@ year = date[2]
 lstm_model = tf.keras.models.load_model('LSTM_MODEL')
 
 #Load the data
-d_data = get_daily_comments(day, month, year, n)
+d_data = get_daily_comments(day, month, year, n, m_c)
 
-#Classification tasks
-daily_pred = classify_comments(d_data, lstm_model)
+try:
+    #Classification tasks
+    daily_pred = classify_comments(d_data, lstm_model)
+    
+    #Daily Analysis
+    daily_stat = daily_stats(daily_pred)
 
-#Daily Analysis
-daily_stat = daily_stats(daily_pred)
+    #Print results
+    print('\n\n\n')
+    print('Daily Discussion Thread Analysis: (Date: %d.%d.%d)'%(day, month, year))
+    print('')
+    print(daily_stat)
 
-#Print results
-print('\n\n\n')
-print('Daily Discussion Thread Analysis: (Date: %d.%d.%d)'%(day, month, year))
-print('')
-print(daily_stat)
-
-
-
- 
+except:
+    print('')
