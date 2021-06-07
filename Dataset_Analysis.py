6ab5cea2743821c1cb527e5e719c5f8cb0517f4d
jaidevsanketi <79723328+jaidevsanketi@users.noreply.github.com>
2021-06-07 11:52:06 +0200

    NEW JD_2

--- Dataset_Analysis.py
+++ Dataset_Analysis.py
@@ -6,25 +6,40 @@ import matplotlib.pyplot as plt
 #Import Dataset class_backtest_df
 data = pd.read_excel('data/class_backtest_df.xlsx')
 data1=data['ticker'].value_counts().head(10)
+print(data)
+
 fig, ax = plt.subplots(figsize=(8, 8))
 res=sns.countplot(data=data, x='ticker', order=data1.index)
-res.set_xticklabels(res.get_xmajorticklabels(), fontsize = 12)
-res.set_yticklabels(res.get_yticks(), size = 15)
-
-ax.set_xlabel('Ticker',size = 15)
-ax.set_ylabel('Count of Mentions',size = 15)
+res.set_xticklabels(res.get_xmajorticklabels())
 
 plt.show()
-'''
+
 data = pd.read_excel('data/dataset.xlsx')
 data1=data['mood'].value_counts()
 fig, ax = plt.subplots(figsize=(8, 8))
 res=sns.countplot(data=data, x='mood', order=data1.index)
-res.set_xticklabels(res.get_xmajorticklabels(), fontsize = 20)
-res.set_yticklabels(res.get_yticks(), size = 12)
-ax.set_ylabel('Count',size = 15)
-ax.set_xlabel('Mood',size = 15)
 
 
 plt.show()
+'''
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# x axis values
+x = ['sun', 'mon', 'fri', 'sat', 'tue', 'wed', 'thu']
+
+# y axis values
+y = [5, 6.7, 4, 6, 2, 4.9, 1.8]
+
+# plotting strip plot with seaborn
+ax = sns.stripplot(x, y);
+
+# giving labels to x-axis and y-axis
+ax.set(xlabel='Days', ylabel='Amount_spend')
+
+# giving title to the plot
+plt.title('My first graph');
+
+# function to show plot
+plt.show()
